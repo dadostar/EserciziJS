237db11f6310e6a032b273b81c0d76881f4bea69
Davide Ceneda <dceneda@yahoo.it>
2012-03-12 16:03:20 +0100

    hw6

--- hw01_puntoetriangolo.js
+++ hw01_puntoetriangolo.js
@@ -68,6 +68,34 @@ var generaNpunti = function (n) {
 	
 }
 
+var bisettrice = function (p) {
+	return p.y - p.x > 0;
+
+}
+var planFilter = function (punti, f) {
+  var pts = punti || [];
+
+  	var res = [];
+
+  for(var i = 0; i < punti.length; i++){
+	if( f(punti[i]) )
+	res.push(punti[i]);
+}
+return res;
+}
+
+Punto.prototype.membership = function (f) {
+	if (f(this)>0) {
+		return 1;
+	}
+	else if (f(this)<0) {
+		return -1;
+		
+	}
+	return 0;
+		
+}
+
 
 
 
