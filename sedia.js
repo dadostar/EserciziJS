9f9e1aaa0cf4429f315b91627c428f7ec86dd2b8
Davide Ceneda <dceneda@yahoo.it>
2012-04-03 10:19:33 +0200

    quasi finita

--- sedia.js
+++ sedia.js
@@ -14,6 +14,10 @@ var sedile1 = SIMPLEX_GRID([[-0.3,-0.05,-0.1,0.05,-0.1,0.05],[-0.3,-0.05,0.4],[-
 
 var sedile2 = SIMPLEX_GRID([[-0.3,-0.05,0.1,-0.05,0.1,-0.05,0.1],[-0.3,-0.05,-0.1,0.05,-0.1,0.05],[-0.45,0.02]]);
 
-var telaio = STRUCT([gambe,telaio2,telaio1,schienale1,schienale2,sedile1,sedile2]);
+
+var sedileLegno = T([0])([0.3])(SIMPLEX_GRID([REPLICA(45)([-0.01,0.001]),[-0.3,-0.05,0.4],[-0.47,0.01]]));
+
+
+var telaio = STRUCT([gambe,telaio2,telaio1,schienale1,schienale2,sedile1,sedile2,sedileLegno]);
 
 DRAW(telaio);
